2b3f52a4f1841c6d710e6d75d368cb0d46117d48
julian gaspar <juliangaspar15@gmail.com>
2023-01-18 19:58:51 +0100

    Added game over logic

--- snake.js
+++ snake.js
@@ -21,34 +21,53 @@ let snake = [
 let actualDirection = DIRECTION.right;
 let newDirection = DIRECTION.right;
 
+let food = generateFood(snake)
+
 /* Draw schema */
 
-function drawGrid(CTX) {
-  for (let x = 20; x < 600; x += 20) {
-    CTX.beginPath();
-    CTX.fillStyle = "black";
-    CTX.moveTo(x, 0);
-    CTX.lineTo(x, 600);
-    CTX.stroke();
-  }
-
-  for (let y = 20; y < 600; y += 20) {
-    CTX.beginPath();
-    CTX.fillStyle = "black";
-    CTX.moveTo(0, y);
-    CTX.lineTo(600, y);
-    CTX.stroke();
-  }
+function fillSquare(CTX, posX, posY) {
+  CTX.beginPath();
+  CTX.fillStyle = "black";
+  CTX.fillRect(posX, posY, 20, 20);
+  CTX.stroke();
 }
 
 function drawSnake(CTX, snake) {
   for (let i = 0; i < snake.length; i++) {
-  CTX.beginPath();
-  CTX.fillStyle = "black";
-  CTX.fillRect(snake[i].posX, snake[i].posY, 20, 20);
-  CTX.stroke();
+    fillSquare(CTX, snake[i].posX, snake[i].posY);
   }
 }
+
+function drawFood(CTX, food) {
+  fillSquare(CTX, food.posX, food.posY);
+}
+
+function drawMap(CTX) {
+  CTX.beginPath();
+  CTX.lineWidth = "2";
+  CTX.rect(20, 20, 560, 560);
+  CTX.stroke();
+}
+
+// Uncoment this function to play with grid
+
+// function drawGrid(CTX) {
+//   for (let x = 20; x < 600; x += 20) {
+//     CTX.beginPath();
+//     CTX.fillStyle = "black";
+//     CTX.moveTo(x, 0);
+//     CTX.lineTo(x, 600);
+//     CTX.stroke();
+//   }
+
+//   for (let y = 20; y < 600; y += 20) {
+//     CTX.beginPath();
+//     CTX.fillStyle = "black";
+//     CTX.moveTo(0, y);
+//     CTX.lineTo(600, y);
+//     CTX.stroke();
+//   }
+// }
 
 /* Move snake */
 
@@ -71,7 +90,44 @@ function moveSnake(movingDirection, snake) {
   snake.pop();
 }
 
-/* Game Cicle */
+/* Generate food */
+
+function generateFood(snake) {
+  while (true) {
+  let columnX = Math.max(Math.floor(Math.random() * 29), 1);
+  let columnY = Math.max(Math.floor(Math.random() * 29), 1);
+
+  let posX = columnX * 20;
+  let posY = columnY * 20;
+
+  let overlap = false;
+  for (let i = 0; i < snake.length; i++) {
+    if (snake[i].posX === posX && snake[i].posY === posY) {
+      overlap = true;
+      break;
+    }
+  }
+  if (overlap) { continue; }
+  return { posX: posX, posY: posY };
+  }
+}
+
+/* Game over */
+
+function wallCollision(snake) {
+  let head = snake[0];
+
+  if ( head.posX < 20 || head.posX >= 580 || head.posY < 20 ||head.posY >= 580) { return true;}
+  if (snake.length === 1) { return false; }
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].posX === head.posX && snake[i].posY === head.posY) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/* Game cicle */
 
 document.addEventListener("keydown", function (event) {
   if (event.code === "ArrowUp" && actualDirection !== DIRECTION.down) {
@@ -88,13 +144,27 @@ document.addEventListener("keydown", function (event) {
 function gameCicle() {
   moveSnake(newDirection, snake);
   actualDirection = newDirection;
+
+  if (snake[0].posX === food.posX && snake[0].posY === food.posY) {
+    snake.push({ posX: snake[snake.length - 1].posX, posY: snake[snake.length - 1].posY });
+    food = generateFood(snake);
+  }
+
+  if (wallCollision(snake)) {
+    clearInterval(cicle);
+    cicle = undefined;
+    return;
+  }
+
   CTX.clearRect(0, 0, 600, 600);
-  drawGrid(CTX);
+  drawMap(CTX);
   drawSnake(CTX, snake);
+  drawFood(CTX, food);
 }
 
-drawGrid(CTX);
+drawMap(CTX);
 drawSnake(CTX, snake);
+drawFood(CTX, food);
 
 CANVAS.addEventListener("click", function () {
   if (cicle === undefined) { cicle = setInterval(gameCicle, FPS); }
